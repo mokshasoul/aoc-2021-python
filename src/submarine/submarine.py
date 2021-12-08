dbaac5ce2ffbab0156b134b0d697fd9a339c5a5d
Charis-Nicolas Georgiou <charis.georgiou@soundcloud.com>
2021-12-08 14:38:56 +0100

    moving to class based declaration

---
+++ src/submarine/submarine.py
@@ -0,0 +1,10 @@
+import re
+import numpy as np
+class Submarine:
+    def __init__(self) -> None:
+        self.depth = 0
+        self.aim_depth = 0
+        self.gamma_rate = 0
+        self.epsilon_rate = 0
+        self.oxygen_generator_rate = 0
+        self.carbon_dioxide_scrubber = 0
