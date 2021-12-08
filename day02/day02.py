6569950aa46409d169d3139580e74e99fe09fae3
Charis-Nicolas Georgiou <charis.georgiou@soundcloud.com>
2021-12-08 11:35:11 +0100

    day02: part two done

--- day02/day02.py
+++ day02/day02.py
@@ -13,18 +13,33 @@ def movement(coords, direction, value):
         print("Wrong directional input")
         return (x,y)
 
+def aim(coords, aim, direction, value):
+    x,y = coords
+    if direction == 'forward':
+        return (x+value, y+aim*value), aim
+    elif direction == 'down':
+        return (x, y), aim+value
+    elif direction == 'up':
+        return (x, y), aim-value
+    else:
+        print("Wrong directional input")
+        return (x,y), aim
+
 depth_coords = (0,0)
+aim_coords = (0,0)
+aim_val = 0
 input_validation = re.compile(r'(forward|down|up) \d+')
 for line in (Path(__file__).parent / 'input.txt').read_text().splitlines():
 # while True:
-    # print("Please enter where submarine should move to:") 
-    # print("Examples: <direction> <value>")
-    # print("To exit please type exit")
-    # user_input = str(input("Please enter now:"))
+#     print("Please enter where submarine should move to:") 
+#     print("Examples: <direction> <value>")
+#     print("To exit please type exit")
+#     user_input = str(input("Please enter now:"))
     user_input = line
     if input_validation.fullmatch(user_input):
         direction, value = user_input.split(" ")
         depth_coords = movement(depth_coords, direction=direction, value=int(value))
+        aim_coords, aim_val = aim(aim_coords, aim=aim_val, direction=direction, value=int(value))
     elif user_input.lower() == "exit":
         break
     else:
@@ -32,4 +47,6 @@ for line in (Path(__file__).parent / 'input.txt').read_text().splitlines():
         continue
 
 x,y = depth_coords
-print(f"FINAL DEPTH: {x*y}")
+x1,y1 = aim_coords
+print(f"FINAL DEPTH PART ONE: {x*y}")
+print(f"FINAL DEPTH PART TWO: {x1*y1}")
